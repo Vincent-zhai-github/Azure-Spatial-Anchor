cb44435794ac04af87e505c8c9ef708164765b5c
Vincent <hfzhai@jingtengtech.com>
2021-06-23 10:43:39 +0800

    add UnityMainThreadDispatcher.cs

--- ASAManager.cs
+++ ASAManager.cs
@@ -8,166 +8,225 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(SpatialAnchorManager))]
-public class ASAManager : MonoBehaviour
+namespace Test
 {
-    private SpatialAnchorManager cloudManager;
-    bool asaStarted = false;
-    bool isErrorActive = false;
-    CloudSpatialAnchor currentCloudAnchor;
-    CloudSpatialAnchorWatcher currentWatcher;
-    TaskCompletionSource<bool> located;
-    public GameObject sphere;
-
-    private List<string> anchorIDs = new List<string>();
-
-    private List<GameObject> anchorGO = new List<GameObject>();
-
-    public Text AnchorID;
-
-    private bool isCreate = false;
-
-    private float recommended;
-
-
-    private int anchorIDNumber = 0;
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(SpatialAnchorManager))]
+    public class ASAManager : MonoBehaviour
     {
-        cloudManager = transform.GetComponent<SpatialAnchorManager>();
-        cloudManager.SpatialAnchorsAccountId = "Your Account Id";
-        cloudManager.SpatialAnchorsAccountDomain = "Your Account Domain";
-        cloudManager.SpatialAnchorsAccountKey = "Your Account Key";
+        private SpatialAnchorManager cloudManager;
+        bool asaStarted = false;
+        bool isErrorActive = false;
+        CloudSpatialAnchor currentCloudAnchor;
+        CloudSpatialAnchorWatcher currentWatcher;
+        public GameObject sphere;
 
-        cloudManager.SessionUpdated += CloudManager_SessionUpdated;
-        cloudManager.AnchorLocated += CloudManager_AnchorLocated;
-        cloudManager.LocateAnchorsCompleted += CloudManager_LocateAnchorsCompleted;
-        cloudManager.LogDebug += CloudManager_LogDebug;
-        cloudManager.Error += CloudManager_Error;
-    }
+        private List<string> anchorIDs = new List<string>();
 
-    private void CloudManager_Error(object sender, SessionErrorEventArgs args)
-    {
-        isErrorActive = true;
+        private List<GameObject> anchorGO = new List<GameObject>();
 
-        Debug.Log("ASA error: " + args.ErrorMessage);
-    }
+        public Text AnchorID;
 
-    private void CloudManager_LogDebug(object sender, OnLogDebugEventArgs args)
-    {
-        Debug.Log("ASA Debug: " + args.Message);
-    }
+        private bool isCreate = false;
 
-    private void CloudManager_LocateAnchorsCompleted(object sender, LocateAnchorsCompletedEventArgs args)
-    {
-        Debug.Log("All anchors located");
-    }
+        private float recommended;
 
-    private void CloudManager_AnchorLocated(object sender, AnchorLocatedEventArgs args)
-    {
-        switch (args.Status)
+        private int anchorIDNumber = 0;
+        // Start is called before the first frame update
+        void Start()
         {
-            case LocateAnchorStatus.Located:
-                UnityMainThreadDispatcher.Instance().Enqueue(() =>
-                {
-                    Debug.Log("ASA Info: Anchor located! Identifier: " + args.Identifier);
+            cloudManager = transform.GetComponent<SpatialAnchorManager>();
+            cloudManager.SpatialAnchorsAccountId = "Your Account Id";
+            cloudManager.SpatialAnchorsAccountDomain = "Your Account Domain";
+            cloudManager.SpatialAnchorsAccountKey = "Your Account Key";
 
-                    CloudSpatialAnchor _cloudSpatialAnchor = args.Anchor;
+            cloudManager.SessionUpdated += CloudManager_SessionUpdated;
+            cloudManager.AnchorLocated += CloudManager_AnchorLocated;
+            cloudManager.LocateAnchorsCompleted += CloudManager_LocateAnchorsCompleted;
+            cloudManager.LogDebug += CloudManager_LogDebug;
+            cloudManager.Error += CloudManager_Error;
+        }
 
-                    GameObject go = Instantiate(sphere);
+        private void CloudManager_Error(object sender, SessionErrorEventArgs args)
+        {
+            isErrorActive = true;
 
-                    CloudNativeAnchor cloudNativeAnchor = go.AddComponent<CloudNativeAnchor>();
-                    cloudNativeAnchor.CloudToNative(_cloudSpatialAnchor);
-                    anchorGO.Add(go);
-                    anchorIDNumber--;
-                    if (anchorIDNumber <= 0)
+            Debug.Log("ASA error: " + args.ErrorMessage);
+        }
+
+        private void CloudManager_LogDebug(object sender, OnLogDebugEventArgs args)
+        {
+            Debug.Log("ASA Debug: " + args.Message);
+        }
+
+        private void CloudManager_LocateAnchorsCompleted(object sender, LocateAnchorsCompletedEventArgs args)
+        {
+            Debug.Log("All anchors located");
+        }
+
+        private void CloudManager_AnchorLocated(object sender, AnchorLocatedEventArgs args)
+        {
+            switch (args.Status)
+            {
+                case LocateAnchorStatus.Located:
+                    UnityMainThreadDispatcher.Instance().Enqueue(() =>
                     {
-                        located.TrySetResult(true);
+                        Debug.Log("ASA Info: Anchor located! Identifier: " + args.Identifier);
+
+                        CloudSpatialAnchor _cloudSpatialAnchor = args.Anchor;
+
+                        GameObject go = Instantiate(sphere);
+
+                        CloudNativeAnchor cloudNativeAnchor = go.AddComponent<CloudNativeAnchor>();
+                        cloudNativeAnchor.CloudToNative(_cloudSpatialAnchor);
+                        anchorGO.Add(go);
+                        anchorIDNumber--;
+                        if (anchorIDNumber <= 0)
+                        {
+                            located.TrySetResult(true);
+                        }
+                    });
+                    break;
+                case LocateAnchorStatus.AlreadyTracked:
+                    Debug.Log("ASA Info: Anchor already tracked. Identifier: " + args.Identifier);
+                    break;
+                case LocateAnchorStatus.NotLocated:
+                    Debug.Log("ASA Info: Anchor not located. Identifier: " + args.Identifier);
+                    break;
+                case LocateAnchorStatus.NotLocatedAnchorDoesNotExist:
+                    Debug.LogError("ASA Error: Anchor not located does not exist. Identifier: " + args.Identifier);
+                    break;
+            }
+        }
+
+        private void CloudManager_SessionUpdated(object sender, SessionUpdatedEventArgs args)
+        {
+            Debug.Log("ASA Log: recommendedForCreate: " + args.Status.RecommendedForCreateProgress);
+            recommended = args.Status.RecommendedForCreateProgress;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// start session
+        /// </summary>
+        /// <returns></returns>
+        private async Task<bool> startASA()
+        {
+            if (!asaStarted)
+            {
+                try
+                {
+                    await cloudManager.CreateSessionAsync();
+                    await cloudManager.StartSessionAsync();
+                    asaStarted = true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log("failed to start ASA " + ex.Message);
+                }
+            }
+
+            return asaStarted;
+        }
+
+        /// <summary>
+        /// stop session
+        /// </summary>
+        private void stopASA()
+        {
+            if (asaStarted)
+            {
+                cloudManager.StopSession();
+                cloudManager.DestroySession();
+                asaStarted = false;
+            }
+        }
+
+        /// <summary>
+        /// 创建Anchor
+        /// </summary>
+        /// <param name="anchorObj">需要同步位置的物体</param>
+        /// <returns></returns>
+        public async Task<string> CreateAnchor(GameObject anchorObj)
+        {
+            bool started = await startASA();
+            if (started)
+            {
+                anchorObj.CreateNativeAnchor();
+
+                CloudSpatialAnchor localCloudAnchor = new CloudSpatialAnchor();
+
+                localCloudAnchor.LocalAnchor = anchorObj.FindNativeAnchor().GetPointer();
+
+                if (localCloudAnchor.LocalAnchor == IntPtr.Zero)
+                {
+                    return null;
+                }
+
+                localCloudAnchor.Expiration = DateTimeOffset.Now.AddDays(7);     //设置创建的cloud anchor 7天有效期
+
+                //等待足够的空间数据
+                while (recommended < 1.0f)
+                {
+                    Debug.Log("wait recommended : " + recommended);
+                    await Task.Delay(330);
+                }
+
+                bool success = false;
+
+                try
+                {
+                    // Actually save
+                    await cloudManager.CreateAnchorAsync(localCloudAnchor);
+
+                    // Store
+                    currentCloudAnchor = localCloudAnchor;
+                    localCloudAnchor = null;
+
+                    // Success?
+                    success = currentCloudAnchor != null;
+
+                    Debug.Log("anchor create success" + success);
+
+                    if (success && !isErrorActive)
+                    {
+                        anchorIDs.Add(currentCloudAnchor.Identifier);
+                        isCreate = false;
+                        return currentCloudAnchor.Identifier;
                     }
-                });
-                break;
-            case LocateAnchorStatus.AlreadyTracked:
-                Debug.Log("ASA Info: Anchor already tracked. Identifier: " + args.Identifier);
-                break;
-            case LocateAnchorStatus.NotLocated:
-                Debug.Log("ASA Info: Anchor not located. Identifier: " + args.Identifier);
-                break;
-            case LocateAnchorStatus.NotLocatedAnchorDoesNotExist:
-                Debug.LogError("ASA Error: Anchor not located does not exist. Identifier: " + args.Identifier);
-                break;
-        }
-    }
-
-    private void CloudManager_SessionUpdated(object sender, SessionUpdatedEventArgs args)
-    {
-        Debug.Log("ASA Log: recommendedForCreate: " + args.Status.RecommendedForCreateProgress);
-        recommended = args.Status.RecommendedForCreateProgress;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /// <summary>
-    /// start session
-    /// </summary>
-    /// <returns></returns>
-    private async Task<bool> startASA()
-    {
-        if (!asaStarted)
-        {
-            try
-            {
-                await cloudManager.CreateSessionAsync();
-                await cloudManager.StartSessionAsync();
-                asaStarted = true;
-            }
-            catch (Exception ex)
-            {
-                Debug.Log("failed to start ASA " + ex.Message);
-            }
-        }
-
-        return asaStarted;
-    }
-
-    /// <summary>
-    /// stop session
-    /// </summary>
-    private void stopASA()
-    {
-        if (asaStarted)
-        {
-            cloudManager.StopSession();
-            cloudManager.DestroySession();
-            asaStarted = false;
-        }
-    }
-
-    /// <summary>
-    /// 创建Anchor
-    /// </summary>
-    /// <param name="anchorObj">需要同步位置的物体</param>
-    /// <returns></returns>
-    public async Task<string> CreateAnchor(GameObject anchorObj)
-    {
-        bool started = await startASA();
-        if (started)
-        {
-            anchorObj.CreateNativeAnchor();
-
-            CloudSpatialAnchor localCloudAnchor = new CloudSpatialAnchor();
-
-            localCloudAnchor.LocalAnchor = anchorObj.FindNativeAnchor().GetPointer();
-
-            if (localCloudAnchor.LocalAnchor == IntPtr.Zero)
-            {
-                return null;
+                    else
+                    {
+                        Debug.Log("failed to create asa anchor");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log("Exception occurred: " + ex.Message);
+                }
             }
 
-            localCloudAnchor.Expiration = DateTimeOffset.Now.AddDays(7);     //设置创建的cloud anchor 7天有效期
+            return null;
+        }
+
+
+        /// <summary>
+        /// 查找Anchor
+        /// </summary>
+        /// <param name="anchorId">要查找的anchor的AnchorId</param>
+        /// <returns></returns>
+        public async void SyncAnchor(string[] anchorIds)
+        {
+            bool started = await startASA();
+            if (!started)
+            {
+                Debug.Log("ASA started failed");
+            }
+
+            anchorIDNumber = anchorIds.Length;
 
             //等待足够的空间数据
             while (recommended < 1.0f)
@@ -176,105 +235,31 @@ public class ASAManager : MonoBehaviour
                 await Task.Delay(330);
             }
 
-            bool success = false;
-
             try
             {
-                // Actually save
-                await cloudManager.CreateAnchorAsync(localCloudAnchor);
-
-                // Store
-                currentCloudAnchor = localCloudAnchor;
-                localCloudAnchor = null;
-
-                // Success?
-                success = currentCloudAnchor != null;
-
-                Debug.Log("anchor create success" + success);
-
-                if (success && !isErrorActive)
+                if ((cloudManager != null) && (cloudManager.Session != null))
                 {
-                    anchorIDs.Add(currentCloudAnchor.Identifier);
-                    isCreate = false;
-                    return currentCloudAnchor.Identifier;
-                }
-                else
-                {
-                    Debug.Log("failed to create asa anchor");
+                    var anchorLocateCriteria = new AnchorLocateCriteria();
+                    anchorLocateCriteria.Identifiers = anchorIds;
+
+                    //创建一个Watcher来查找anchor
+                    currentWatcher = cloudManager.Session.CreateWatcher(anchorLocateCriteria);
                 }
             }
             catch (Exception ex)
             {
-                Debug.Log("Exception occurred: " + ex.Message);
+                Debug.Log("failed sync anchor: " + ex.Message);
             }
         }
 
-        return null;
-    }
-
-
-    /// <summary>
-    /// 同步Anchor
-    /// </summary>
-    /// <param name="anchorId">从服务端获取到的AnchorId</param>
-    /// <returns></returns>
-    public async Task<bool> SyncAnchor(string[] anchorIds)
-    {
-        bool started = await startASA();
-        if (!started)
+        public void LoadAnchors()
         {
-            Debug.Log("ASA started failed");
-            return false;
+            SyncAnchor(anchorIDs.ToArray());
         }
 
-        anchorIDNumber = anchorIds.Length;
-        located = new TaskCompletionSource<bool>();
-
-        //等待足够的空间数据
-        while (recommended < 1.0f)
+        public void DeleteAnchor(GameObject deleteAnchorGO)
         {
-            Debug.Log("wait recommended : " + recommended);
-            await Task.Delay(330);
+            deleteAnchorGO.DeleteNativeAnchor();
         }
-
-        try
-        {
-            if ((cloudManager != null) && (cloudManager.Session != null))
-            {
-                var anchorLocateCriteria = new AnchorLocateCriteria();
-                anchorLocateCriteria.Identifiers = anchorIds;
-
-                //创建一个Watcher来查找anchor
-                currentWatcher = cloudManager.Session.CreateWatcher(anchorLocateCriteria);
-
-                bool success = await located.Task;
-                located = null;
-
-                if (success)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.Log("failed sync anchor: " + ex.Message);
-        }
-
-        return false;
-    }
-
-    public async void LoadAnchors()
-    {
-        await SyncAnchor(anchorIDs.ToArray());
-    }
-
-    public void DeleteAnchor(GameObject deleteAnchorGO)
-    {
-        deleteAnchorGO.DeleteNativeAnchor();
     }
 }
